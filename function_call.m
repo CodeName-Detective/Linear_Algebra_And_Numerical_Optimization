aa362805b3bc3457ff422c64240326bc633524c6
Shri <shrithegreat18@gmail.com>
2025-01-07 18:08:39 -0500

    Delete function_call.m

--- function_call.m
+++
@@ -1,9 +0,0 @@
-function [b] = sadapala_pp1(a)
-    % sadapala_pp1 simply returns the input value as output of the function
-    % output = input
-
-    % a(input): input value that is passed into the function
-    % b(output): output value returned as the output of the function 
-
-    b = a; % assign the input value to the output
-end % sadapala_pp1
